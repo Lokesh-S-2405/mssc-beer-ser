d26e64be11e8f1f7b85ebeaf2d34208a12e16fc9
Loki <lokes9686@gmail.com>
2021-07-20 09:38:36 +0530

    initial project add.commit 1

---
+++ src/main/java/com/example/msscbeerser/MsscBeerSerApplication.java
@@ -0,0 +1,13 @@
+package com.example.msscbeerser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBeerSerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscBeerSerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/msscbeerser/MsscBeerSerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msscbeerser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerSerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
