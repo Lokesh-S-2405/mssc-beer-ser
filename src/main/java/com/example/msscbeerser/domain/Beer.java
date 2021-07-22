6c8b20d359c8d244d138f264817cb7dee00dbbcf
Loki <lokes9686@gmail.com>
2021-07-22 08:30:30 +0530

    addded beerloader

--- src/main/java/com/example/msscbeerser/domain/Beer.java
+++ src/main/java/com/example/msscbeerser/domain/Beer.java
@@ -3,11 +3,9 @@ package com.example.msscbeerser.domain;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.util.UUID;
@@ -25,11 +23,15 @@ public class Beer {
     @GenericGenerator(name="UUID",strategy = "org.hibernate.id.UUIDGenerator")
     @Column(length = 36,columnDefinition = "varchar",updatable = false,nullable = false)
     private UUID id;
+
+    @Version
     private Long version;
 
     @CreationTimestamp
     @Column(updatable = false)
     private Timestamp createdDate;
+
+    @UpdateTimestamp
     private Timestamp lastModifiedDate;
     private String beerName;
     private String beerStyle;
