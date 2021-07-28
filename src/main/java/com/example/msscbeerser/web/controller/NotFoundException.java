44ae3b9fb6b84b09e5077aedb237cf3030352b5b
Loki <lokes9686@gmail.com>
2021-07-28 11:11:52 +0530

    adding service for controller

---
+++ src/main/java/com/example/msscbeerser/web/controller/NotFoundException.java
@@ -0,0 +1,4 @@
+package com.example.msscbeerser.web.controller;
+
+public class NotFoundException extends RuntimeException{
+}
--- src/main/java/com/example/msscbeerser/web/model/BeerDto.java
+++ src/main/java/com/example/msscbeerser/web/model/BeerDto.java
@@ -1,5 +1,6 @@
 package com.example.msscbeerser.web.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -22,8 +23,10 @@ public class BeerDto {
     @Null
     private Integer version;
     @Null
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
     @Null
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
     private OffsetDateTime lastModifiedDate;
     @NotBlank
     private String beerName;
@@ -32,6 +35,8 @@ public class BeerDto {
     @Positive
     @NotNull
     private Long upc;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     @Positive
     @NotNull
     private BigDecimal price;
